e069846b397247a9755f7c8f12231335dec11287
BalitskiyAnton <Bondarenko03130@gmail.com>
2023-01-06 01:47:22 +0100

    Update

---
+++ app/src/main/java/com/test/cryptoappproject/adapters/CoinInfoAdapter.kt
@@ -0,0 +1,10 @@
+package com.test.cryptoappproject.adapters
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class CoinInfoAdapter {
+    inner class CoinInfoViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
+}
